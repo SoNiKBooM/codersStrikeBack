79ea8fce2c7bdafdf06118534df412bc8a26f230
SoNiKBooM <sonik.td@gmail.com>
2016-09-23 12:50:13 +1000

    Added angle and distance as two variables to control speed better.
    -Still yet to tweak numbers

--- bronze1.js
+++ bronze1.js
@@ -16,121 +16,70 @@ while (true) {
     var inputs = readline().split(' ');
     var opponentX = parseInt(inputs[0]);
     var opponentY = parseInt(inputs[1]);
-    var thrust = '';
+    var thrust = parseInt(accel(angleThrust,distThrust));
+    var angleThrust = 0;
+    var angleDist = 0;
     // Write an action using print()
     // To debug: printErr('Debug messages...');
 
 
-    if (nextCheckpointDist > 5000) { //LONG DISTANCE MANUVEURE
 
-        if (nextCheckpointAngle < -100 || nextCheckpointAngle > 100) { // 150Deg Angle Manuveure
-            var thrust = 0;
-        }
+    //CHECKPOINT ANGLE
 
-
-        else if (nextCheckpointAngle < -90 || nextCheckpointAngle > 90) { // 90Deg Angle Manuveure
-            var thrust = 50;
-        }
-
-        else if (nextCheckpointAngle < -67.5 || nextCheckpointAngle > 67.5) { // 67.5Deg Angle Manuveure
-            var thrust = 80;
-        }
-
-        else if (nextCheckpointAngle < -45 || nextCheckpointAngle > 45) { // 45Deg Angle Manuveure
-            var thrust = 90;
-        }
-
-        else if (nextCheckpointAngle < -22.5 || nextCheckpointAngle > 22.5) { //22.5Deg Angle Manuveure
-            var thrust = 95;
-        }
-
-        else if (nextCheckpointAngle === 0 || nextCheckpointAngle === 0) { //22.5Deg Angle Manuveure
-            var thrust = 'BOOST';
-        }
-
-
+    if (nextCheckpointAngle < -100 || nextCheckpointAngle > 100) { // 150Deg Angle Manuveure
+        var angleThrust = 0;
     }
 
-    else if (nextCheckpointDist > 3000 && nextCheckpointDist < 5000) { //MEDIUM DISTANCE MANUVEURE
 
-        if (nextCheckpointAngle < -180 || nextCheckpointAngle > 180) { // 180Deg Angle Manuveure
-            var thrust = 0;
-        }
-
-        else if (nextCheckpointAngle < -110 || nextCheckpointAngle > 110) { // 110Deg Angle Manuveure
-            var thrust = 10;
-        }
-
-        else if (nextCheckpointAngle < -90 || nextCheckpointAngle > 90) { // 110Deg Angle Manuveure
-            var thrust = 20;
-        }
-
-        else if (nextCheckpointAngle < -67.5 || nextCheckpointAngle > 67.5) { // 67.5Deg Angle Manuveure
-            var thrust = 50;
-        }
-
-        else if (nextCheckpointAngle < -45 || nextCheckpointAngle > 45) { // 45Deg Angle Manuveure
-            var thrust = 80;
-        }
-
-        else if (nextCheckpointAngle < -22.5 || nextCheckpointAngle > 22.5) { //22.5Deg Angle Manuveure
-            var thrust = 90;
-        }
-
-        else if (nextCheckpointAngle === 0 || nextCheckpointAngle === 0) {
-            var thrust = 95;
-        }
-
+    else if (nextCheckpointAngle < -90 || nextCheckpointAngle > 90) { // 90Deg Angle Manuveure
+        var angleThrust = 50;
     }
 
-    else if (nextCheckpointDist < 3000 && nextCheckpointDist > 1500) { //SHORT DISTANCE MANUVEURE
-
-
-
-        if (nextCheckpointAngle < -110 || nextCheckpointAngle > 110) { // 110Deg Angle Manuveure
-            var thrust = 0;
-        }
-
-        else if (nextCheckpointAngle < -75 || nextCheckpointAngle > 75) { // 67.5Deg Angle Manuveure
-            var thrust = 50;
-        }
-
-        else if (nextCheckpointAngle < -67.5 || nextCheckpointAngle > 67.5) { // 67.5Deg Angle Manuveure
-            var thrust = 70;
-        }
-
-        else if (nextCheckpointAngle < -45 || nextCheckpointAngle > 45) { // 45Deg Angle Manuveure
-            var thrust = 80;
-        }
-
-        else if (nextCheckpointAngle < -22.5 || nextCheckpointAngle > 22.5) { //22.5Deg Angle Manuveure
-            var thrust = 90;
-        }
-
-        else if (nextCheckpointAngle > -22.5 || nextCheckpointAngle < 22.5) { //22.5Deg Angle Manuveure
-            var thrust = 95;
-        }
-
-
+    else if (nextCheckpointAngle < -67.5 || nextCheckpointAngle > 67.5) { // 67.5Deg Angle Manuveure
+        var angleThrust = 80;
     }
 
-    /*else if (nextCheckpointDist < 1600 && nextCheckpointDist > 1200) {
-     var thrust = thrust - (thrust * .3);
-     }
+    else if (nextCheckpointAngle < -45 || nextCheckpointAngle > 45) { // 45Deg Angle Manuveure
+        var angleThrust = 90;
+    }
 
-     else if (nextCheckpointDist < 1200 && nextCheckpointDist > 400) {
-     var thrust = thrust - (thrust * .5);
-     }
+    else if (nextCheckpointAngle < -22.5 || nextCheckpointAngle > 22.5) { //22.5Deg Angle Manuveure
+        var angleThrust = 95;
+    }
 
-     else if (nextCheckpointDist < 400 && nextCheckpointDist > 0) {
-     var thrust = thrust - (thrust * 7);
-     }*/
-
-
+    else if (nextCheckpointAngle === 0 || nextCheckpointAngle === 0) { //22.5Deg Angle Manuveure
+        var thrust = 'BOOST';
+    }
 
 
-    else {
-        thrust = 100;
+    //CHECKPOINT DISTANCE
+
+    if (nextCheckpointDist > 8000) {
+        var distThrust = 100;
+    }
+
+    else if (nextCheckpointDist > 5000) {
+        var thrust = 'BOOST';
+    }
+
+    else if (nextCheckpointDist > 3000) {
+        var distThrust = 100;
+    }
+
+    else if (nextCheckpointDist > 2000) {
+        var distThrust = 80;
+    }
+
+    else if (nextCheckpointDist > 1000) {
+        var distThrust = 60;
+    }
+
+    else if (nextCheckpointDist > 600) {
+        var distThrust = 45;
+    }
+
+    else if (nextCheckpointDist > 400) {
+        var distThrust = 40;
     }
 
 
@@ -138,5 +87,18 @@ while (true) {
     // You have to output the target position
     // followed by the power (0 <= thrust <= 100) or "BOOST"
     // i.e.: "x y thrust"
-    print(nextCheckpointX + ' ' + nextCheckpointY + ' ' + thrust;
+
+    function accel(angleThrust,distThrust) {
+        if (angleThrust == 'BOOST' && distThrust == 100) {
+            return 'BOOST'
+        }
+        else {
+            return (angleThrust + distThrust) / 2;
+        }
+
+    }
+
+
+
+    print(nextCheckpointX + ' ' + nextCheckpointY + ' ' + thrust);
 }
