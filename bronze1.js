abcd053f697ddbb7090a378b9f22e97748ad806a
SoNiKBooM <sonik.td@gmail.com>
2016-09-23 16:42:00 +1000

    -Numbers tweaked for Level_1 for ~80th percintile wins.

--- bronze1.js
+++ bronze1.js
@@ -35,12 +35,12 @@ while (true) {
             return parseInt((a + b) / 2);
         }
 
-        else if (a !== parseInt(a) && b !== parseInt(b)) {
+        else if ((a !== parseInt(a)) && (b !== parseInt(b))) {
             return 'BOOST';
         }
 
-        else if (a !== parseInt(a) || b !== parseInt(b)) {
-            if (a !== parseInt(a) || b === parseInt(b)) {
+        else if ((a !== parseInt(a)) || (b !== parseInt(b))) {
+            if ((a !== parseInt(a)) && (b === parseInt(b))) {
                 return parseInt((b + 100) / 2);
             }
             else {
@@ -55,60 +55,86 @@ while (true) {
 
     //CHECKPOINT ANGLE
 
-    if (nextCheckpointAngle < -100 || nextCheckpointAngle > 100) { // 150Deg Angle Manuveure
+    if (nextCheckpointAngle <= -180 || nextCheckpointAngle >= 180) { // More than 150Deg Angle Manuveure
+        var angleThrust = 0;
+        var distThrust = 0;
+    }
+
+    else if (nextCheckpointAngle <= -160 || nextCheckpointAngle >= 160) { // 90Deg Angle Manuveure
+        var angleThrust = 0;
+        var distThrust = 0;
+    }
+
+    else if (nextCheckpointAngle <= -140 || nextCheckpointAngle >= 140) { // 67.5Deg Angle Manuveure
+        var angleThrust = 0;
+        var distThrust = 0;
+    }
+
+    else if (nextCheckpointAngle <= -120 || nextCheckpointAngle >= 120) { // 45Deg Angle Manuveure
+        var angleThrust = 0;
+        var distThrust = 0;
+    }
+
+    else if (nextCheckpointAngle <= -100 || nextCheckpointAngle >= 100) { //Between 0 and 45Deg Angle
         var angleThrust = 0;
     }
 
+    else if (nextCheckpointAngle <= -80 || nextCheckpointAngle >= 80) { //Between 0 and 45Deg Angle
+        var angleThrust = 25;
+    }
 
-    else if (nextCheckpointAngle < -90 || nextCheckpointAngle > 90) { // 90Deg Angle Manuveure
+    else if (nextCheckpointAngle <= -60 || nextCheckpointAngle >= 60) { //Between 0 and 45Deg Angle
         var angleThrust = 50;
     }
 
-    else if (nextCheckpointAngle < -67.5 || nextCheckpointAngle > 67.5) { // 67.5Deg Angle Manuveure
-        var angleThrust = 80;
+    else if (nextCheckpointAngle <= -40 || nextCheckpointAngle >= 40) { //Between 0 and 45Deg Angle
+        var angleThrust = 75;
     }
 
-    else if (nextCheckpointAngle < -45 || nextCheckpointAngle > 45) { // 45Deg Angle Manuveure
+    else if (nextCheckpointAngle <= -20 || nextCheckpointAngle >= 20) { //Between 0 and 45Deg Angle
         var angleThrust = 90;
     }
 
-    else if (nextCheckpointAngle < -22.5 || nextCheckpointAngle > 22.5) { //22.5Deg Angle Manuveure
-        var angleThrust = 95;
+    else if (nextCheckpointAngle <= -0 || nextCheckpointAngle >= 0) { //Straight On
+        if (nextCheckpointDist > 5000) {
+            var angleThrust = 'BOOST'
+        }
+        else {
+            var angleThrust = 100;
+        }
     }
 
-    else if (nextCheckpointAngle === 0 || nextCheckpointAngle === 0) { //22.5Deg Angle Manuveure
-        var distThrust = 'BOOST';
+    else if (nextCheckpointAngle === 0) { //Straight On and distance good
+        var angleThrust = 100;
     }
+
+
 
 
     //CHECKPOINT DISTANCE
 
-    if (nextCheckpointDist > 8000) {
-        var distThrust = 'BOOST';
-    }
-
-    else if (nextCheckpointDist > 5000) {
-        var distThrust = 'BOOST';
-    }
-
-    else if (nextCheckpointDist > 3000) {
+    if (nextCheckpointDist > 3000) {
         var distThrust = 100;
     }
 
-    else if (nextCheckpointDist > 2000) {
-        var distThrust = 80;
+    else if (nextCheckpointDist <= 4000 && nextCheckpointDist >= 3000) {
+        var distThrust = 75;
     }
 
-    else if (nextCheckpointDist > 1000) {
-        var distThrust = 60;
+    else if (nextCheckpointDist < 3000 && nextCheckpointDist >= 2000) {
+        var distThrust = 50;
     }
 
-    else if (nextCheckpointDist > 600) {
-        var distThrust = 45;
+    else if (nextCheckpointDist < 1000 && nextCheckpointDist >= 500) {
+        var distThrust = 25;
     }
 
-    else if (nextCheckpointDist > 400) {
-        var distThrust = 40;
+    else if (nextCheckpointDist < 500 && nextCheckpointDist >= 200) {
+        var distThrust = 0;
+    }
+
+    else if (nextCheckpointDist < 200) {
+        var distThrust = 0;
     }
 
 
